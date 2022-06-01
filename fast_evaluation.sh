8bcfeb716bc02fa53b9c1d779fd2b8fecf2f499c
Chaithanya67 <97585371+Chaithanya67@users.noreply.github.com>
2022-06-01 16:28:22 +0530

    Update fast_evaluation.sh

--- fast_evaluation.sh
+++ fast_evaluation.sh
@@ -1,2 +1,3 @@
 chmod u+x /content/Vertex-Clustering/main.py /content/Vertex-Clustering/datasets /content/Vertex-Clustering/datasets/study  &&
 python3 /content/Vertex-Clustering/evaluation_metrics.py --clustered prediction.csv --goldstandard datasets/study_ground_truth.csv
+
