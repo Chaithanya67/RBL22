cf1e1ccc02bdde1c985edaf53d09869d1c59282d
Chaithanya67 <97585371+Chaithanya67@users.noreply.github.com>
2022-06-01 15:54:19 +0530

    Update fast_evaluation.sh

--- fast_evaluation.sh
+++ fast_evaluation.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python3 main.py datasets study 1 &&
-python3 evaluation_metrics.py --clustered prediction.csv --goldstandard datasets/study_ground_truth.csv
+python3 main.py dataset study 1 &&
+python3 evaluation_metrics.py --clustered prediction.csv --goldstandard dataset/study_ground_truth.csv
